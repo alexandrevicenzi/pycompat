14b2fcab73eb1eba86ab83347aaca47daea6bb8d
Alexandre Vicenzi <vicenzi.alexandre@gmail.com>
2014-07-04 13:22:34 -0300

    Fix test.

--- tests/test.py
+++ tests/test.py
@@ -13,7 +13,7 @@ class TestPyCompat(unittest.TestCase):
         pass
 
     def test_python_is_64bits(self):
-        self.assertEqual(python.is_64bits, not python.is_32bits)
+        self.assertEqual(py.is_64bits, not py.is_32bits)
 
     # Is failing on Travis-CI Python 3+
     #'def test_immutability(self):
