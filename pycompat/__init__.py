2c97815a0d588229d57d139110d13af2c18d5846
Alexandre Vicenzi <vicenzi.alexandre@gmail.com>
2014-10-04 17:18:26 -0300

    Remove wrong code.

--- pycompat/__init__.py
+++ pycompat/__init__.py
@@ -83,10 +83,7 @@ class _ImmutableObject:
 
     def __setattr__(self, name, value):
         if self.__dict__.get(name):
-            raise.is_alpha
-.is_beta
-.is_candidate
-.is_final AttributeError('Constant attribute "%s" cannot be reassigned.' % name)
+            raise AttributeError('Constant attribute "%s" cannot be reassigned.' % name)
         else:
             self.__dict__[name] = value
 
